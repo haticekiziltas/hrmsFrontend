6572356b8e4f67ee0b8e92d7ba60c23bed3c16e6
Hatice Kızıltaş <81980876+haticekiziltas@users.noreply.github.com>
2021-06-27 22:19:12 +0300

    req 23/ 97.06.2021

--- src/pages/JobAdvertisements/JobAdvertisementList.jsx
+++ src/pages/JobAdvertisements/JobAdvertisementList.jsx
@@ -1,29 +1,62 @@
 import React, { useEffect, useState } from 'react'
 import JobAdvertisementService from '../../services/jobAdvertisementService'
-import { Item, Container,Segment,Label,Button } from 'semantic-ui-react'
+import { Item, Container,Segment,Label, Pagination} from 'semantic-ui-react'
+import {Select} from 'evergreen-ui'
 import City from '../City'
 import JobPosition from '../JobPosition'
 
 export default function JobAdvertisementList() {
 
+  function calculateDay(value) {
+
+    if (value) {
+      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+      if (seconds < 29) // 30 saniyeden önceyse 'şimdi' yazar
+          return 'Şimdi';
+      const intervals = {
+          'yıl': 31536000,
+          'ay': 2592000,
+          'hafta': 604800,
+          'gün': 86400,
+          'saat': 3600,
+          'dakika': 60,
+          'saniye': 1
+      };
+      let counter;
+      for (const i in intervals) {
+          counter = Math.floor(seconds / intervals[i]);
+          if (counter > 0)
+              if (counter === 1) {
+                  return counter + ' ' + i + ' önce'; // tekil (1 gün önce)
+              } else {
+                  return counter + ' ' + i + ' önce'; // çoğul (2 veya daha fazla gün öncesi)
+              }
+      }
+  }
+  return value;
+  }
+
 
   const [jobAdvertisement, setJobAdvertisements] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-
   const [filters, setFilters] = useState('')
-  const handleFilters = (filters, category) => {
+
+  const handleFilters = (filters) => {
   setFilters(filters)
+}
 
-  }
-  const handleClick = function (e) {
-    console.log(e.target.value);
+  // const handleClick = function (e) {
+  //   console.log(e.target.value);
 
-  };
+  // };
 
+ 
+  const [pageNo, setActivePage] = useState(1);
+  const handlePaginationChange = (e, { activePage }) => setActivePage(activePage)
+  const [pageSize, setPageSize] = useState(10);
 
   useEffect(() => {
     let jobAdvertisementService = new JobAdvertisementService();
-    jobAdvertisementService.getJobAdvertisements().then(result => setJobAdvertisements(result.data.data))
+    jobAdvertisementService.getPageable(pageNo, pageSize).then(result => setJobAdvertisements(result.data.data))
   }, [jobAdvertisement])
 
   const itemStyle = {
@@ -36,22 +69,21 @@ export default function JobAdvertisementList() {
 
   }
 
-
   return (
 
     <Segment basic style={{maxWidth:850,}}>
         <JobPosition handleFilters={filters => handleFilters(filters, "continents")}></JobPosition>
-     <City handleFilters={filters => handleFilters(filters, "continents")}></City>
+      <City handleFilters={filters => handleFilters(filters, "continents")}></City>
   
 {
     jobAdvertisement.filter(value => {
-      if (setSearchTerm === '') {
+      if (setFilters=== '') {
           return value
       } else if (value.jobName.toLocaleLowerCase().includes(filters.toLocaleLowerCase()) || value.cityName.toLocaleLowerCase().includes(filters.toLocaleLowerCase())){
 
           return value
     }
-      }).map(jobAdvertisement, index => (
+      }).map(jobAdvertisement => (
   
   <Item.Group key={jobAdvertisement.id}  divided>
  
@@ -76,7 +108,7 @@ export default function JobAdvertisementList() {
         {jobAdvertisement.cityName}
       </Item.Description>
       <Item.Description>
-        {jobAdvertisement.description}
+        {jobAdvertisement.jobDescription}
       </Item.Description>
       <Container style={{display:"flex",justifyContent:"space-between",alignItems:"left",right:30}}>
       <Item.Extra>
@@ -86,7 +118,7 @@ export default function JobAdvertisementList() {
 
       <Item.Extra >
       <Label style={{borderRadius:20, margin: "auto",
-    marginLeft: "30px",fontWeight:600}} icon='time' content="7 gün önce"/>
+    marginLeft: "30px",fontWeight:600}} icon='time' content={calculateDay(jobAdvertisement.createdAt)}/>
        
       </Item.Extra>
       </Container>
@@ -96,6 +128,21 @@ export default function JobAdvertisementList() {
 </Item.Group>
 
  ))}
+  <Select onChange={e => setPageSize(e.target.value)} width={240} height={40}>
+        <option  value="10" selected>
+          10
+        </option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </Select>
+      <Pagination
+
+        activePage={pageNo}
+        onPageChange={handlePaginationChange}
+        totalPages={pageSize}
+        ellipsisItem={null}
+      />
              
  </Segment>
 
--- src/services/jobAdvertisementService.js
+++ src/services/jobAdvertisementService.js
@@ -2,18 +2,22 @@ import axios from './axios/axios'
 
 export default class jobAdvertisementService {
     getJobAdvertisements() {
-        return axios.get("/jobAdvertisment/getActive")
+        return axios.get("/jobAdvertisement/getActive")
     };
 
     jobAdvertisementadd(values) {
-        return axios.post("/jobAdvertisment/addAd",values)
+        return axios.post("/jobAdvertisement/addAd",values)
     };
 
     getAllPassiveJobAdvertisements() {
-        return axios.get("/jobAdvertisment/getAllPassive")
+        return axios.get("/jobAdvertisement/getAllPassive")
     };
     getAllUpdateActiveJobAdvertisements(id, active) {
-        return axios.put("/jobAdvertisment/updateActive?active=" +active +"&id=" +id)
+        return axios.put("/jobAdvertisement/updateActive?active=" +active +"&id=" +id)
+    };
+
+    getPageable(pageNo,pageSize) {
+        return axios.get("/jobAdvertisement/getAllByPage?pageNo="+pageNo+"&pageSize="+pageSize)
     };
 }
 
